e1d5a1fcb3a7f289be832472ce03fb95ae2e464d
TRK41 <Troyk@bell.net>
2023-02-10 13:45:36 -0500

    fixing bugs in .env and models

--- models/Product.js
+++ models/Product.js
@@ -37,7 +37,7 @@ Product.init(
     catergory_id: {
       type: DataTypes.INTEGER,
       references: {
-        model: 'catergory',
+        model: 'category',
         key: 'id',
       },
     },
