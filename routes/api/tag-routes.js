6854ed78b8bd45ba3b7f528f588401f3f1eece77
TRK41 <Troyk@bell.net>
2023-02-10 16:49:59 -0500

    finished routes

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -57,8 +57,22 @@ router.put('/:id', async(req, res) => {
   }
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async(req, res) => {
   // delete on tag by its `id` value
+  try{
+    const tagData = await Tag.destroy(req.body, {
+      where: {
+        id: req.params.id
+      }
+    });
+    if(!tagData){
+      res.status(404).json({Message: "No tag with this ID"})
+      return;
+    }
+    res.status(500).json(err);
+  }catch(err){
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
