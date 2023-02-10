8b68ab796c044dac988118421dbbab8962dfb421
TRK41 <Troyk@bell.net>
2023-02-10 13:04:46 -0500

    finish code in models index.js

--- models/index.js
+++ models/index.js
@@ -4,13 +4,23 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 const sequelize = require('../config/connection.js');
+
 // Products belongsTo Category
-
+Product.belongsTo(Category,{
+  foreignKey: 'category_id',
+});
 // Categories have many Products
-
+Category.hasMany(Product,{
+  foreignKey: 'category_id'
+});
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+});
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+});
 
 module.exports = {
   Product,
